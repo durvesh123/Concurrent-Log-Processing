348c57f16cd0c9ddff9f935b789fc2329308e4ac
Durvesh Vedak <durveshvedak@Durveshs-MBP.fios-router.home>
2019-06-09 17:13:39 -0400

    * added concurrent implementations of reading from database and processing statistics

--- src/main/java/com/verizon/multithreaded/database/Controller.java
+++ src/main/java/com/verizon/multithreaded/database/Controller.java
@@ -1,6 +1,9 @@
 package com.verizon.multithreaded.database;
 
 
+import com.verizon.model.Statistics;
+import com.verizon.singlethreaded.SingleThreadedVideoStatistics;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,6 +18,11 @@ public class Controller {
         DatabaseConcurrency dbc = new DatabaseConcurrency(connection, user, password, concurrencyFactor );
         List<String> input = Arrays.asList("log1.txt","log2.txt","log3.txt");
         dbc.loadDatabase(input);
+        List<Statistics> stats = dbc.getModelFromDatabase();
+        SingleThreadedVideoStatistics svs = new SingleThreadedVideoStatistics();
+        svs.display( dbc.sortedByNumberOfViews(input) );
+        svs.display( dbc.sortedByCategoryTitleVideoLength(input) );
+        svs.display( dbc.videosWatchedOver50Percent(input) );
         dbc.stopExecution();
 
     }
--- src/main/java/com/verizon/multithreaded/database/DatabaseConcurrency.java
+++ src/main/java/com/verizon/multithreaded/database/DatabaseConcurrency.java
@@ -2,12 +2,12 @@ package com.verizon.multithreaded.database;
 
 import com.verizon.model.Statistics;
 import com.verizon.singlethreaded.SingleThreadedVideoStatistics;
+import com.verizon.multithreaded.future.CompletableFutureImplementation;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class DatabaseConcurrency {
 
@@ -15,9 +15,12 @@ public class DatabaseConcurrency {
     private final String user;
     private final String password;
     private Connection connect;
+    private Statement statement = null;
+    private ResultSet resultSet = null;
     private final SingleThreadedVideoStatistics svs;
     private final int concurrencyFactor;
     private final ExecutorService executorService;
+    private final CompletableFutureImplementation completableFutureImplementation;
 
     public DatabaseConcurrency(String connection, String user, String password, int concurrencyFactor) {
         this.connection = connection;
@@ -26,9 +29,12 @@ public class DatabaseConcurrency {
         this.svs = new SingleThreadedVideoStatistics();
         this.concurrencyFactor = concurrencyFactor;
         this.executorService = Executors.newFixedThreadPool(concurrencyFactor);
+        this.completableFutureImplementation = new CompletableFutureImplementation(this.concurrencyFactor);
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             this.connect = DriverManager.getConnection(connection,user,password);
+            this.statement = connect.createStatement();
+
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
@@ -37,8 +43,6 @@ public class DatabaseConcurrency {
     }
 
     public void loadDatabase( List<String> fileNames ) {
-
-
 
         executorService.submit(new Runnable() {
             @Override
@@ -73,11 +77,109 @@ public class DatabaseConcurrency {
 
     }
 
+    public List<Statistics> getModelFromDatabase() {
+
+        Future<List<Statistics>> completableFuture = new CompletableFuture<>();
+        completableFuture = executorService.submit(new Callable<List<Statistics>>() {
+            @Override
+            public List<Statistics> call() throws Exception {
+                List<Statistics> stats = new ArrayList<>();
+                try {
+                    resultSet = statement.executeQuery("select * from verizon.stats");
+                    while ( resultSet.next() ) {
+                        String category = resultSet.getString("category");
+                        String title = resultSet.getString("title");
+                        Double video_length = resultSet.getDouble("video_length");
+                        Double average_watch_time = resultSet.getDouble("average_watch_time");
+                        Integer number_of_views = resultSet.getInt("number_of_views");
+                        Statistics s = new Statistics(category, title, video_length, average_watch_time, number_of_views);
+                        stats.add(s);
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                return stats;
+            }
+        });
+
+        try {
+            return completableFuture.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+
+
+    public List<Statistics> sortedByNumberOfViews( List<String> input ) {
+
+        Future<List<Statistics>> completableFuture = new CompletableFuture<>();
+         completableFuture = executorService.submit(new Callable<List<Statistics>>() {
+             @Override
+             public List<Statistics> call() throws Exception {
+                 return completableFutureImplementation.sortedByNumberOfViews(input);
+             }
+         });
+
+        try {
+            return completableFuture.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public List<Statistics> sortedByCategoryTitleVideoLength( List<String> input ) {
+        Future<List<Statistics>> completableFuture = new CompletableFuture<>();
+        completableFuture = executorService.submit(new Callable<List<Statistics>>() {
+            @Override
+            public List<Statistics> call() throws Exception {
+                return completableFutureImplementation.sortedByCategoryTitleVideoLength(input);
+            }
+        });
+
+        try {
+            return completableFuture.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public List<Statistics> videosWatchedOver50Percent( List<String> input ) {
+        Future<List<Statistics>> completableFuture = new CompletableFuture<>();
+        completableFuture = executorService.submit(new Callable<List<Statistics>>() {
+            @Override
+            public List<Statistics> call() throws Exception {
+                return completableFutureImplementation.videosWatchedOver50Percent(input);
+            }
+        });
+
+        try {
+            return completableFuture.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
     public void stopExecution() {
+
         executorService.shutdown();
         try {
+            connect.close();
             executorService.awaitTermination(1, TimeUnit.DAYS);
-        } catch (InterruptedException e) {
+        } catch (InterruptedException | SQLException e) {
             e.printStackTrace();
         }
     }
